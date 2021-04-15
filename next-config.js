78dd1b9b7c029a850bbc325c230003d1d779924f
luiys <luisgustavofeitoza@gmail.com>
2021-04-15 19:02:23 -0300

    Update Boilerplate

--- next-config.js
+++
@@ -1,9 +0,0 @@
-import withPWA from 'next-pwa'
-const isProd = process.env.NODE_ENV === 'production'
-
-export default withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
---
+++ next.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
